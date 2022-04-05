8f69881da1bf93ff5bc5c85b1498c86beeca60ab
Ayoon Kim <ayoonkim7@gmail.com>
2022-04-05 18:24:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Toronto's Homicide Occurrences 
+
+We use the Open Data Toronto - available here: https://open.toronto.ca/ -> 
+	Recently added datasets -> Police Annual Statistical Report - Homicides. That raw data is saved in inputs/data. 
---
+++ scripts/01-data_cleaning.R
@@ -0,0 +1,35 @@
+#### Preamble ####
+# Purpose: Clean and prepare the data 
+# Author: Ayoon Kim
+# Data: 3 January 2021
+# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# License: MIT
+# Pre-requisites: 
+# - Need to have downloaded the ACS data and saved it to inputs/data
+# - Don't forget to gitignore it!
+# - Change these to yours
+# Any other information needed?
+
+
+#### Workspace setup ####
+# Use R Projects, not setwd().
+library(haven)
+library(tidyverse)
+# Read in the raw data. 
+raw_data <- readr::read_csv("inputs/data/homicide.csv")
+# Just keep some variables that may be of interest (change 
+# this depending on your interests)
+names(raw_data)
+
+reduced_data <- 
+  raw_data %>% 
+  select(first_col, 
+         second_col)
+rm(raw_data)
+         
+
+#### What's next? ####
+
+
+
+         
